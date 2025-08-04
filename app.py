c3567f3bbf480a43e2ba7aaa6104d524a814003f
Eldrick0410 <eldrick@momentum-commerce.com>
2025-08-04 15:23:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if file1 and file2:
             "product id", 
             "product ad impressions", 
             "product ad clicks", 
-            "orders", 
+            "orders (SKU)", 
             "gross revenue"
         }
 
@@ -103,4 +103,5 @@ if file1 and file2:
 
     except Exception as e:
         st.error(f"❌ Error processing files: {e}")
+
 
